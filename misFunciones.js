884510493cd7ab7a2063308c0f5a412f26c879d2
JoaquinRomeroVillagra <118319483+JoaquinRomeroVillagra@users.noreply.github.com>
2023-06-05 20:01:25 -0300

    Update misFunciones.js(entrada-salida-convertir)

--- misFunciones.js
+++ misFunciones.js
@@ -8,44 +8,44 @@
  * @param {number} valor - El valor de los imputs de dolares, libras, reales, pesos o euros.
  * @return
  */
-function conversorMonedas(id, valor) {
-    if (valor.includes(",")) {
-        valor = valor.replace(",", ".");
-    }
-    if (isNaN(valor)) {
-        document.lasMonedasDelMundo.unid_dolar.value = "";
-        document.lasMonedasDelMundo.unid_libras.value = "";
-        document.lasMonedasDelMundo.unid_reales.value = "";
-        document.lasMonedasDelMundo.unid_euro.value = "";
-        document.lasMonedasDelMundo.unid_peso.value = "";
-        alert("El valor ingresado es incorrecto");
-    } else if (id == "Dolar-(Estadounidense)") {
-        document.lasMonedasDelMundo.unid_libras.value = Math.round(valor * 0.83 * 100) / 100;
-        document.lasMonedasDelMundo.unid_reales.value = Math.round(valor * 5.1 * 100) / 100;
-        document.lasMonedasDelMundo.unid_euro.value = Math.round(valor * 0.93 * 100) / 100;
-        document.lasMonedasDelMundo.unid_peso.value = Math.round(valor * 240 * 100) / 100;
-    } else if (id == "Libras-Esterlinas") {
-        document.lasMonedasDelMundo.unid_dolar.value = Math.round(valor * 1.20 * 100) / 100;
-        document.lasMonedasDelMundo.unid_euro.value = Math.round(valor * 1.12 * 100) / 100;
-        document.lasMonedasDelMundo.unid_reales.value = Math.round(valor * 6.12 * 100) / 100;
-        document.lasMonedasDelMundo.unid_peso.value = Math.round(valor * 300 * 100) / 100;
-    } else if (id == "Reales") {
-        document.lasMonedasDelMundo.unid_dolar.value = Math.round(valor * 0.19 * 100) / 100;
-        document.lasMonedasDelMundo.unid_libras.value = Math.round(valor * 0.16 * 100) / 100;
-        document.lasMonedasDelMundo.unid_euro.value = Math.round(valor * 0.18 * 100) / 100;
-        document.lasMonedasDelMundo.unid_peso.value = Math.round(valor * 47 * 100) / 100;
-    } else if (id == "Euros") {
-        document.lasMonedasDelMundo.unid_dolar.value = Math.round(valor * 1.07 * 100) / 100;
-        document.lasMonedasDelMundo.unid_libras.value = Math.round(valor * 0.89 * 100) / 100;
-        document.lasMonedasDelMundo.unid_reales.value = Math.round(valor * 5.45 * 100) / 100;
-        document.lasMonedasDelMundo.unid_peso.value = Math.round(valor * 258 * 100) / 100;
-    } else if (id == "Peso-Argentino") {
-        document.lasMonedasDelMundo.unid_dolar.value = Math.round(valor * 0.0041 * 100) / 100;
-        document.lasMonedasDelMundo.unid_libras.value = Math.round(valor * 0.0033 * 100) / 100;
-        document.lasMonedasDelMundo.unid_euro.value = Math.round(valor * 0.0038 * 100) / 100;
-        document.lasMonedasDelMundo.unid_reales.value = Math.round(valor * 0.0213 * 100) / 100;
-    }
-}
+// function conversorMonedas(id, valor) {
+//     if (valor.includes(",")) {
+//         valor = valor.replace(",", ".");
+//     }
+//     if (isNaN(valor)) {
+//         document.lasMonedasDelMundo.unid_dolar.value = "";
+//         document.lasMonedasDelMundo.unid_libras.value = "";
+//         document.lasMonedasDelMundo.unid_reales.value = "";
+//         document.lasMonedasDelMundo.unid_euro.value = "";
+//         document.lasMonedasDelMundo.unid_peso.value = "";
+//         alert("El valor ingresado es incorrecto");
+//     } else if (id == "Dolar-(Estadounidense)") {
+//         document.lasMonedasDelMundo.unid_libras.value = Math.round(valor * 0.83 * 100) / 100;
+//         document.lasMonedasDelMundo.unid_reales.value = Math.round(valor * 5.1 * 100) / 100;
+//         document.lasMonedasDelMundo.unid_euro.value = Math.round(valor * 0.93 * 100) / 100;
+//         document.lasMonedasDelMundo.unid_peso.value = Math.round(valor * 240 * 100) / 100;
+//     } else if (id == "Libras-Esterlinas") {
+//         document.lasMonedasDelMundo.unid_dolar.value = Math.round(valor * 1.20 * 100) / 100;
+//         document.lasMonedasDelMundo.unid_euro.value = Math.round(valor * 1.12 * 100) / 100;
+//         document.lasMonedasDelMundo.unid_reales.value = Math.round(valor * 6.12 * 100) / 100;
+//         document.lasMonedasDelMundo.unid_peso.value = Math.round(valor * 300 * 100) / 100;
+//     } else if (id == "Reales") {
+//         document.lasMonedasDelMundo.unid_dolar.value = Math.round(valor * 0.19 * 100) / 100;
+//         document.lasMonedasDelMundo.unid_libras.value = Math.round(valor * 0.16 * 100) / 100;
+//         document.lasMonedasDelMundo.unid_euro.value = Math.round(valor * 0.18 * 100) / 100;
+//         document.lasMonedasDelMundo.unid_peso.value = Math.round(valor * 47 * 100) / 100;
+//     } else if (id == "Euros") {
+//         document.lasMonedasDelMundo.unid_dolar.value = Math.round(valor * 1.07 * 100) / 100;
+//         document.lasMonedasDelMundo.unid_libras.value = Math.round(valor * 0.89 * 100) / 100;
+//         document.lasMonedasDelMundo.unid_reales.value = Math.round(valor * 5.45 * 100) / 100;
+//         document.lasMonedasDelMundo.unid_peso.value = Math.round(valor * 258 * 100) / 100;
+//     } else if (id == "Peso-Argentino") {
+//         document.lasMonedasDelMundo.unid_dolar.value = Math.round(valor * 0.0041 * 100) / 100;
+//         document.lasMonedasDelMundo.unid_libras.value = Math.round(valor * 0.0033 * 100) / 100;
+//         document.lasMonedasDelMundo.unid_euro.value = Math.round(valor * 0.0038 * 100) / 100;
+//         document.lasMonedasDelMundo.unid_reales.value = Math.round(valor * 0.0213 * 100) / 100;
+//     }
+// }
 
 y=0
 dy = 1;
@@ -62,4 +62,58 @@ function animarBillete(){
         ctx.drawImage(img, 10,y);
     }
 y+=dy;
+}
+function conversorMonedas() {
+    var monedaEntrada = document.getElementById("Moneda-de-entrada").value;
+    var monedaSalida = document.getElementById("Moneda-de-salida").value;
+    var cantidad = document.getElementById("Cantidad").value;
+
+    if (isNaN(cantidad)) {
+        document.getElementById("resultado").textContent = "";
+        alert("El valor ingresado es incorrecto");
+    } else {
+        if (monedaEntrada === monedaSalida) {
+            document.getElementById("resultado").textContent = cantidad;
+        } else if (monedaEntrada === "Dolar-(Estadounidense)") {
+            if (monedaSalida === "Libras-Esterlinas") {
+                document.getElementById("resultado").textContent = Math.round(cantidad * 0.83 * 100) / 100;
+            } else if (monedaSalida === "Reales") {
+                document.getElementById("resultado").textContent = Math.round(cantidad * 5.1 * 100) / 100;
+            } else if (monedaSalida === "Euros") {
+                document.getElementById("resultado").textContent = Math.round(cantidad * 0.93 * 100) / 100;
+            } else if (monedaSalida === "Peso-Argentino") {
+                document.getElementById("resultado").textContent = Math.round(cantidad * 240 * 100) / 100;
+            }
+        } else if (monedaEntrada === "Libras-Esterlinas") {
+            if (monedaSalida === "Dolar-(Estadounidense)") {
+                document.getElementById("resultado").textContent = Math.round(cantidad * 1.20 * 100) / 100;
+            } else if (monedaSalida === "Euros") {
+                document.getElementById("resultado").textContent = Math.round(cantidad * 1.12 * 100) / 100;
+            } else if (monedaSalida === "Reales") {
+                document.getElementById("resultado").textContent = Math.round(cantidad * 6.12 * 100) / 100;
+            } else if (monedaSalida === "Peso-Argentino") {
+                document.getElementById("resultado").textContent = Math.round(cantidad * 300 * 100) / 100;
+            }
+        } else if (monedaEntrada === "Reales") {
+            if (monedaSalida === "Dolar-(Estadounidense)") {
+                document.getElementById("resultado").textContent = Math.round(cantidad * 0.19 * 100) / 100;
+            } else if (monedaSalida === "Libras-Esterlinas") {
+                document.getElementById("resultado").textContent = Math.round(cantidad * 0.16 * 100) / 100;
+            } else if (monedaSalida === "Euros") {
+                document.getElementById("resultado").textContent = Math.round(cantidad * 0.18 * 100) / 100;
+            } else if (monedaSalida === "Peso-Argentino") {
+                document.getElementById("resultado").textContent = Math.round(cantidad * 47 * 100) / 100;
+            }
+        } else if (monedaEntrada === "Euros") {
+            if (monedaSalida === "Dolar-(Estadounidense)") {
+                document.getElementById("resultado").textContent = Math.round(cantidad * 1.07 * 100) / 100;
+            } else if (monedaSalida === "Libras-Esterlinas") {
+                document.getElementById("resultado").textContent = Math.round(cantidad * 0.89 * 100) / 100;
+            } else if (monedaSalida === "Reales") {
+                document.getElementById("resultado").textContent = Math.round(cantidad * 5.45 * 100) / 100;
+            } else if (monedaSalida === "Peso-Argentino") {
+                document.getElementById("resultado").textContent = Math.round(cantidad * 258 * 100) / 100;
+            }
+        }
+    }
 }
